06327f3d908537e7b324c9b97367374b0c1520a2
JavaEdge <sss2855845850@gmail.com>
2023-06-15 15:53:19 +0800

    feat:使用递归组件实现详情页列表

--- vue-cli/Travel/src/pages/detail/components/List.vue
+++ vue-cli/Travel/src/pages/detail/components/List.vue
@@ -1,15 +1,26 @@
+<!--这段代码是一个Vue组件，名为DetailList
+模板部分定义了一个div，其中包含一个v-for循环，循环遍历list数组中的每一个元素
+将其渲染为一个item。每个item包含一个标题和一个可能存在的子列表
+如果item有子列表，就会将其传递给另一个名为detail-list的组件进行渲染
+脚本部分定义了DetailList组件，其props接受一个名为list的数组参数
+样式部分定义了一些CSS样式，用于美化组件的外观。-->
+
+
+<!--5. 在脚本中，定义DetailList组件，其props接受一个名为list的数组参数。-->
+<!--6. 样式部分定义了一些CSS样式，用于美化组件的外观。-->
 <template>
   <div>
-    <div
-      class="item"
-      v-for="(item, index) of list"
-      :key="index"
-    >
+    <!--2. 在模板中定义一个div，其中包含一个v-for循环
+        循环遍历list数组中的每一个元素，将其渲染为一个item。-->
+    <!--3. 每个item包含一个标题和一个可能存在的子列表。-->
+    <div class="item" v-for="(item, index) of list" :key="index">
+      <!-- 第一层循环 -->
       <div class="item-title border-bottom">
         <span class="item-title-icon"></span>
         {{item.title}}
       </div>
-      <div v-if="item.children" class="item-chilren">
+      <!--4. 如果item有子列表，就会将其传递给另一个名为detail-list的组件进行渲染。-->
+      <div v-if="item.children" class="item-children">
         <detail-list :list="item.children"></detail-list>
       </div>
     </div>
@@ -40,6 +51,6 @@ export default {
     line-height: .8rem
     font-size: .32rem
     padding: 0 .2rem
-  .item-chilren
+  .item-children
     padding: 0 .2rem
 </style>
